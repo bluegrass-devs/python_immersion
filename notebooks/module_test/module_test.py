6b6837d1b848bf65e8b51c148f0e429874640e86
Joe Blankenship <joe.blankenship1@gmail.com>
2018-09-16 12:37:23 -0400

    update to module_test example - part 2

--- notebooks/module_test/module_test.py
+++ notebooks/module_test/module_test.py
@@ -1,10 +1,21 @@
-'''
+#!/usr/bin/env python3
+"""
 This is a basic test module for the Python course. It has 1 function:
     
     hello_world
     
 See methods' docs for more details
-'''
+"""
+
+__author__ = "Joe"
+__version__ = "0.1.0"
+__license__ = "CC 4.0"
+
 
 def hello_world():
-    print('Hello!')
+    """demo function"""
+    print('Hello World!')
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    hello_world()
